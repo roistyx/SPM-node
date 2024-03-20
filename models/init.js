fc1eb48ade12af41d693d30b109d2b8bcecbf4ea
Roie <roee.abram@gmail.com>
2024-03-20 18:38:34 -0400

    UPDATED: Updated app.js and package-lock.json, removed code from models/init.js.
    - In app.js:
      - Updated require statements to use single quotes instead of double quotes.
      - Removed unused code importing InitDB and InitDBAtlas.
      - Updated the origin value in the cors middleware.
      - Changed the static file path to use single quotes instead of double quotes.
      - Updated the route path in the app.get() method.
    
    - In models/init.js:
      - Commented out the entire file as it is no longer used.
    
    - In package-lock.json:
      - Updated the name field to "SPM-Node" instead of "my-jimp-app".

--- models/init.js
+++ models/init.js
@@ -1,16 +1,16 @@
-const { MongoClient } = require("mongodb");
-const FinancialsDao = require("./FinancialsDao");
-const StockNewsDAO = require("./StockNewsDao");
+// const { MongoClient } = require("mongodb");
+// const FinancialsDao = require("./FinancialsDao");
+// const StockNewsDAO = require("./StockNewsDao");
 
-module.exports.InitDB = async function initDB() {
-  MongoClient.connect(process.env.MONGODB_URI)
+// module.exports.InitDB = async function initDB() {
+//   MongoClient.connect(process.env.MONGODB_URI)
 
-    .then(async (connection) => {
-      await StockNewsDAO.injectDB(connection.db(process.env.DB));
-      await FinancialsDao.injectDB(connection.db(process.env.DB));
-    })
-    .catch((err) => {
-      console.log("error connecting to MongoDB:", err);
-      process.exit(1);
-    });
-};
+//     .then(async (connection) => {
+//       await StockNewsDAO.injectDB(connection.db(process.env.DB));
+//       await FinancialsDao.injectDB(connection.db(process.env.DB));
+//     })
+//     .catch((err) => {
+//       console.log("error connecting to MongoDB:", err);
+//       process.exit(1);
+//     });
+// };
